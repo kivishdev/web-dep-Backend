62bc5fb99a3d5225804e1949fe0ad83f0f477ab1
Kirti Yadav <kirtiy380@gmail.com>
2025-10-18 17:51:37 +0530

    Learned to Create Individual middleware  Route

--- Express/express.js
+++ Express/express.js
@@ -1,9 +1,13 @@
 // To install Express: npm i express
 // Express ek toolbox jaise hai jisse server create and manage kar sakte hai
-
 const express = require("express");
+const morgan = require("morgan");  // Requiring Morgan
+// Opening Express Toolkit
 const app = express(); // Ye express ko on kar deta hai
 
+app.use(morgan("dev"))  // Morgan se key detaiils maang rhaa.
+
+// View Engine: 
 // Ab HTML ko render krnee k liye we will use "ejs". Setting the view engine to EJS
 // Is line se Node.js ko bataya jata hai ki humara templating engine EJS hai 
 // Matlab jab hum res.render() likhenge, Express "views" folder me .ejs file dhundhega
@@ -25,8 +29,24 @@ app.use((req,res,next) => {
     return next()    // Iska mtlb ye haii aage badho.
 })
 
+// Learning third party middleware: Jo hum koii baahar ka code use krtee hai 
+// For Heree we used  morgan.
+
+// Hum chahte hai to hum ek middleware apne 1 individual route k liyee bnaa sktee haiii 
+app.get("/",(req,res,next) => {
+    console.log("This is the individual middleware for the Home Page that is at route /");
+   let a = 23;
+   let b = 3;
+   console.log(a+b)
+   next();
+}, (request,response) => {
+    response.send('This is the home page at route "/')
+});
+
+
+//  Express Routes Example:
 // Ex. usage of express server and render a html page:
-app.get("/", (req, res) => { 
+app.get("/index", (req, res) => { 
     // If sirf text bhejna ho to send() use karo
     // res.send("This is the Home Page");
 
