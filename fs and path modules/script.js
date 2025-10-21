5eaf0f4830542f800205f41c341b50633457f02a
Kirti Yadav <kirtiy380@gmail.com>
2025-10-22 02:09:28 +0530

    Learned fs and its some of basic methods.

---
+++ fs and path modules/script.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+
+// Creating a file:
+// Syntax: fs.writeFile("file-ka-naam","Uske andar ka content", (error)=>{
+//     if(err) {
+//         console.log("There's and Error");
+//     }
+// })
+
+
+fs.writeFile("script.txt", "Hello haa mai andar se create huaa hu", (err)=> {
+    if(err) console.log(err, "This is the Error");
+        else console.log("Done Creating");
+    })
+
+// Appendfile: Mtlb likhe hue me aage add krnaa
+// Syntax: fs.writeFile("script.txt", "Hello haa mai andar se create huaa hu", (err)=> {
+    // if(err) console.log(err, "This is the Error");
+    //     else console.log("Done Creating");
+    // })
+
+fs.appendFile("script.txt", "Haa Haa vahii se CreateFile and Appendfile se", (err)=> {
+    if(err) console.log(err, "This is the Error");
+        else console.log("Done Creating");
+    })
+
+// Rename File:
+// Syntax: fs.rename("old-file", "new-file", (err) => {
+//     if(err) console.log(err);
+//     else console.log("Renamed");
+// })
+
+fs.rename("script.txt", "new-script.txt", (err) => {
+    if(err) console.log(err);
+    else console.log("Renamed");
+})
+
+// CopyFile: Old file ko new file me copy krnaaa
+// Syntax: fs.rename("file-jisko copy krnaa haii", "target-file", (err) => {
+//     if(err) console.log(err);
+//     else console.log("Renamed");
+// })
+fs.copyFile("new-script.txt", "chacha.txt", (err) => {
+    if(err) console.log(err);
+    else console.log("Copied Successfullyy");
+})
+
+// Unlink: Removes the file:
+fs.unlink("new-script.txt", (err)=> {
+    if(err) console.log(err.errno, err.code, "This is the error");
+    else console.log("Removed");
+})
+
+// Readfile: File content padhna
+fs.readFile("script.js", (err,data) => {
+    if(err) console.log(err)
+    else console.log(data.toString())
+})
