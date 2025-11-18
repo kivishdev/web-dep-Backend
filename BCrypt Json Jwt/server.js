c205249645a82b220dde244bf3f4e53348c01c1e
Kirti Yadav <kirtiy380@gmail.com>
2025-11-19 00:10:15 +0530

    Setting up bcrypt and jwt for learning

---
+++ BCrypt Json Jwt/server.js
@@ -0,0 +1,5 @@
+// Sbsee pehle khuchh cheeze sikhni haiii alag alag
+// Cookies kaise set krtee hai 
+// BCrypt kaise use krtee hai and encryption and decryption kaise hota hai.
+// JWT kyaa hai and jwt me data kaise store karee and bahar nikaale.
+
--- auth/server.js
+++
@@ -1,7 +0,0 @@
-// Authentication and Authorization:
-// Authentication: User ko access dene se pehle verify krnaa ki user hai kon.
-
-// Authorization: Login k baad user ko konsi resource pe access rhegaa 
-
-
-
