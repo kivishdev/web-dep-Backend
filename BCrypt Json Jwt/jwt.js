5e624b2973ecb7de6e64cf25f43e7a0f013afc00
Kirti Yadav <kirtiy380@gmail.com>
2025-11-22 19:17:55 +0530

    Learned Signing and Verifying Jwt

--- BCrypt Json Jwt/jwt.js
+++ BCrypt Json Jwt/jwt.js
@@ -1,9 +1,34 @@
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+// Syntax: jwt.sign( PAYLOAD, SECRET_KEY, OPTIONS )
+    // const token = jwt.sign(
+    //     { email: user.email, id: user._id }, // 1. Payload (Data)
+    //     "mera_secret_key_bhai_kisi_ko_mt_btana", // 2. Secret Key
+    //     { expiresIn: "1h" } // 3. Options (Expiry)
+    // );
 
 app.get("/", (req, res) => {
-
+    let token = jwt.sign({email: "data-need-to-sent"}, "PRIVATE-KEY" )
+    res.cookie("token", token)
+    console.log(token);
+    res.send("Done");
 })
 
+app.get('/read', (req,res) => {
+    // To Read jwt data:
+    let jwtdata = jwt.verify(req.cookies.token, "key")
+    console.log(jwtdata);
+    
+    res.send("Yaa Data Received")
+})
+
+app.get('/del', (req,res) => {
+    res.clearCookie("token");
+    res.send("Cookie Deleted");
+})
 
 app.listen(3000);
